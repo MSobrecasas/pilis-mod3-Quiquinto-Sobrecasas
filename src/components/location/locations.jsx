f7dc2893ab42d92ff8a2f8595c2cf4677d781740
MSobrecasas <sns.mary@hotmail.com>
2022-10-10 20:24:14 -0300

    fix minibugs

--- src/components/location/locations.jsx
+++ src/components/location/locations.jsx
@@ -4,24 +4,45 @@ import { LocationContext } from "../../contexts/LocationContext";
 import { useContext } from "react";
 
 const Locations = ({}) => {
-  const locations = useContext(LocationContext);
+  const locations = useContext(LocationContext);  
   console.log("dashboiard Vale" + JSON.stringify(locations));
   console.log("dashboiard le Vale: " + locations.tarjeta.length);
+  
+  
+  const locationsCopy = locations;
+  console.log("copy "+ JSON.stringify( locationsCopy))
+  //const { tarjeta, setTarjeta } = useContext(LocationContext)
+  let handleChange = (e) => {
+     const search = e.target.value;
+    console.log(e.target.value)
+   /*  setTarjeta(locationsCopy.tarjeta.filter((location) => {
+     location.name.toLowerCase().includes(search.toLowerCase());
+    }) ) */
+  };
 
   return (
-    <div className="grid">
-      {locations.tarjeta.length > 0 ? (
-        locations.tarjeta.map((location) => (
-          <Location key={location.id} location={location} />
-        ))
-      ) : (
-        <div>
-          <h1 className="mensaje-agregar-tarjeta">
-            agregar tarjeta de ubicación...
-          </h1>
-        </div>
-      )}
-    </div>
+    <section>
+      <div className="input__busqueda">
+        <input
+          type="text"
+          placeholder="Ingresar ciudad"
+          onChange={e =>handleChange(e)}
+        />
+      </div>
+      <div className="grid">
+        {locations.tarjeta.length > 0 ? (
+          locations.tarjeta.map((location) => (
+            <Location key={location.id} location={location} />
+          ))
+        ) : (
+          <div>
+            <h1 className="mensaje-agregar-tarjeta">
+              agregar tarjeta de ubicación...
+            </h1>
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
--- src/routes/Card/CardCreation.jsx
+++ src/routes/Card/CardCreation.jsx
@@ -1,73 +1,76 @@
-import { useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { LocationContext } from '../../contexts/LocationContext';
-import { getClima } from '../../service';
-import './CardCreation.css';
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { LocationContext } from "../../contexts/LocationContext";
+import { getClima } from "../../service";
+import "./CardCreation.css";
 
 const CardCreation = () => {
-  const { tarjeta, setTarjeta } = useContext(LocationContext) //context
+  const { tarjeta, setTarjeta } = useContext(LocationContext); //context
   const navigate = useNavigate();
-  
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    defaultValues: { 
-     }
+    defaultValues: {},
   });
 
-  const onSubmit = (data) =>{
+  const onSubmit = (data) => {
     getAux(data);
-    navigate('/')
+    navigate("/");
   };
 
   let getAux = (formData) => {
-    getClima(formData.locationlatitude,formData.locationlongitude)
-    .then((data) => {
-      const locationNew = {
-        id: tarjeta[tarjeta.length - 1].id + 1,
-        name: formData.locationName,
-        latitude: data.latitude,
-        longitude: data.longitude,
-        current_weather: data.current_weather,
-        daily: data.daily,
-        imagen: formData.imagen,
-        deleted : false
-      };
-      setTarjeta([...tarjeta, locationNew])
-      navigate('/')
-    }) 
-    .catch((err) => console.log(err));  
+    getClima(formData.locationlatitude, formData.locationlongitude)
+      .then((data) => {
+        const locationNew = {
+          id: 1,
+          name: formData.locationName,
+          latitude: data.latitude,
+          longitude: data.longitude,
+          current_weather: data.current_weather,
+          daily: data.daily,
+          imagen: formData.imagen,
+          deleted: false,
+        };
+        if (tarjeta.length != 0) {
+         locationNew.id = tarjeta[tarjeta.length -1 ].id + 1;
+         console.log("entra")
+        }
+        setTarjeta([...tarjeta, locationNew]);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
-    <div className='location-new-container'>
+    <div className="location-new-container">
       <span>Crea una nueva ubicación</span>
-      <form className='location-form' onSubmit={handleSubmit(onSubmit)}>
+      <form className="location-form" onSubmit={handleSubmit(onSubmit)}>
         <input
-          className='input-location-name-form'
-          type='text'
-          placeholder='Nombre de la ubicacion'
-          {...register('locationName', {
-            required: 'Debe ingresar un nombre',
+          className="input-location-name-form"
+          type="text"
+          placeholder="Nombre de la ubicacion"
+          {...register("locationName", {
+            required: "Debe ingresar un nombre",
           })}
         />
         <p>{errors.locationName?.message}</p>
         <input
-          type='text'
-          placeholder='Latitude'
-          {...register('locationlatitude', {
-            required: 'Debe ingresar una lalitud',
+          type="text"
+          placeholder="Latitude"
+          {...register("locationlatitude", {
+            required: "Debe ingresar una lalitud",
           })}
         />
         <p>{errors.locationlatitude?.message}</p>
         <input
-          type='text'
-          placeholder='Longitude'
-          {...register('locationlongitude', {
-            required: 'Debe ingresar una longitud',
+          type="text"
+          placeholder="Longitude"
+          {...register("locationlongitude", {
+            required: "Debe ingresar una longitud",
           })}
         />
         <p>{errors.locationlongitude?.message}</p>
@@ -79,7 +82,7 @@ const CardCreation = () => {
           })}
         />
         <p>{errors.imagen?.message}</p>
-        <button className='btn-form' type='submit'>
+        <button className="btn-form" type="submit">
           Crear Ubicación
         </button>
       </form>
@@ -87,4 +90,4 @@ const CardCreation = () => {
   );
 };
 
-export default CardCreation; 
+export default CardCreation;
